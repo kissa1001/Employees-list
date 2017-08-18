0884722c97cae7edef3eda373f8d91d02d1ce831
kissa1001 <kissa101295@gmail.com>
2017-08-18 15:37:26 -0700

    added files

---
+++ server/main.js
@@ -0,0 +1,22 @@
+import _ from 'lodash';
+import { Meteor } from 'meteor/meteor';
+import { Employees } from '../imports/collections/employees';
+import { image, helpers } from 'faker';
+
+Meteor.startup( () => {
+  const numberRecords = Employees.find({}).count();
+  if(!numberRecords) {
+    _.times(5000, () => {
+      const { name, email, phone } = helpers.createCard();
+      Employees.insert({
+        name,
+        email,
+        phone,
+        avatar: image.avatar()
+      })
+    })
+  }
+  Meteor.publish('employees', function() {
+    return Employees.find({}, { limit: 20 });
+  });
+});
